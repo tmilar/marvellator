52abd45a86a89f901c6da070c8bba4baee58ab62
tmilar <tomasmilar4@gmail.com>
2016-04-21 20:34:44 -0300

    Add findFirstByUserName method to UserRepository to be able to search an user by username

--- src/main/java/org/utn/marvellator/repository/UserRepository.java
+++ src/main/java/org/utn/marvellator/repository/UserRepository.java
@@ -3,9 +3,8 @@ package org.utn.marvellator.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.utn.marvellator.model.User;
 
-/**
- * Created by Tomas on 4/20/2016.
- */
-
 public interface UserRepository extends MongoRepository<User, String>{
+
+    User findFirstByUserName(String userName);
+
 }
