fa858226408dfc52aa524116b8a118272733212c
tmilar <tomasmilar4@gmail.com>
2016-04-21 21:24:31 -0300

    Add tests for findFirstByUserName repository

--- src/test/java/org/utn/marvellator/repository/UserRepositoryTest.java
+++ src/test/java/org/utn/marvellator/repository/UserRepositoryTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.utn.marvellator.MarvellatorApplicationTestConfig;
 import org.utn.marvellator.model.User;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MarvellatorApplicationTestConfig.class)
@@ -25,9 +27,25 @@ public class UserRepositoryTest {
 
     @Test
     public void shouldSaveNewUsers() {
-        userRepository.save(new User("fede"));
+        userRepository.save(new User("test1"));
         assertEquals(1, userRepository.count());
     }
 
+    @Test
+    public void shouldFindUserByUserName() {
+        String testUsername = "test1";
+        userRepository.save(new User(testUsername));
 
+        User foundUser = userRepository.findFirstByUserName(testUsername);
+
+        assertNotNull(foundUser);
+        assertEquals(testUsername, foundUser.getUserName());
+    }
+
+    @Test(expected = DuplicateKeyException.class)
+    public void shouldNotSaveAnUserWithAnExistingUsername(){
+        String testUsername = "test1";
+        userRepository.save(new User(testUsername));
+        userRepository.save(new User(testUsername));
+    }
 }
