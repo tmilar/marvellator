18cd44c343a6ce22ae482b146755934f4c35b1c5
tmilar <tomasmilar4@gmail.com>
2016-04-21 21:21:05 -0300

    Correctly use test configuration in UserRepositoryTest

--- src/test/java/org/utn/marvellator/repository/UserRepositoryTest.java
+++ src/test/java/org/utn/marvellator/repository/UserRepositoryTest.java
@@ -7,15 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.utn.marvellator.MarvellatorApplication;
-import org.utn.marvellator.MarvellatorApplicationTests;
+import org.utn.marvellator.MarvellatorApplicationTestConfig;
 import org.utn.marvellator.model.User;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MarvellatorApplication.class)
+@SpringApplicationConfiguration(classes = MarvellatorApplicationTestConfig.class)
 public class UserRepositoryTest {
 
     @Autowired
